a94292984e384639d1783586632711cd46a939b1
Ênio Henrique <62400466+ribeirowski@users.noreply.github.com>
2023-12-01 10:03:02 -0300

    update: finished dark mode

--- PTA/Desafio de Natal/script.js
+++ PTA/Desafio de Natal/script.js
@@ -1,38 +1,39 @@
 var flag = 0;
 function darkMode() {
+    var elemento_body = document.body;
+    var elemento_menu = document.getElementsByClassName("menu");
+    var elemento_p = document.getElementsByClassName("text-p");
+    var elemento_gift_name = document.getElementsByClassName("gift-name");
+    var elemento_gift_price = document.getElementsByClassName("gift-price");
+    var elemento_item = document.getElementsByClassName("item");
+    var elemento_theme_button = document.getElementsByClassName("theme-mode");
+    elemento_body.classList.toggle("dark-mode-body");
+    for (var i = 0; i < elemento_menu.length; i++){
+        elemento_menu[i].classList.toggle("dark-mode-menu");
+    }
+    for (var i = 0; i < elemento_p.length; i++){
+        elemento_p[i].classList.toggle("dark-mode-p");
+    }
+    for (var i = 0; i < elemento_item.length; i++){
+        elemento_item[i].classList.toggle("dark-mode-item");
+    }
+    for (var i = 0; i < elemento_theme_button.length; i++){
+        elemento_theme_button[i].classList.toggle("dark-theme-mode");
+    }
     if (flag == 0){
+        for (var i = 0; i < elemento_gift_name.length; i++){
+            elemento_gift_name[i].style.color = "#C2BDBD";
+            elemento_gift_price[i].style.color = "#F3F2F2";
+        }
         changeImageLight('change-theme');
         flag++;
     } else {
+        for (var i = 0; i < elemento_gift_name.length; i++){
+            elemento_gift_name[i].style.color = "#5D5656";
+            elemento_gift_price[i].style.color = "#282525";
+        }
         changeImageDark('change-theme');
         flag = 0;
-    }
-    var element1 = document.body;
-    var element2 = document.getElementsByClassName("menu");
-    var element3 = document.getElementsByClassName("text-p");
-    var element4 = document.getElementsByClassName("item");
-    var element5 = document.getElementsByClassName("gift-name");
-    var element6 = document.getElementsByClassName("gift-price");
-    var element7 = document.getElementsByClassName("theme-mode");
-    element1.classList.toggle("dark-mode-body");
-    for (var i = 0; i < element2.length; i++){
-        element2[i].classList.toggle("dark-mode-menu");
-    }
-    for (var i = 0; i < element3.length; i++){
-        element3[i].classList.toggle("dark-mode-p");
-    }
-    for (var i = 0; i < element4.length; i++){
-        console.log(element4[i]);
-        element4[i].classList.toggle("dark-item");
-    }
-    for (var i = 0; i < element5.length; i++){
-        element5[i].classList.toggle("dark-mode-gitf-name");
-    }
-    for (var i = 0; i < element6.length; i++){
-        element6[i].classList.toggle("dark-mode-gitf-price");
-    }
-    for (var i = 0; i < element7.length; i++){
-        element7[i].classList.toggle("dark-theme-mode");
     }
 }
 
