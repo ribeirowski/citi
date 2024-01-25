933f3394ee097e961f97e41759181a3276f69424
Ênio Henrique <62400466+ribeirowski@users.noreply.github.com>
2024-01-25 02:55:41 -0300

    desafioFAI: included font.ts

---
+++ Desafio FAI/web/src/styles/fonts.ts
@@ -0,0 +1,6 @@
+import { Barlow } from 'next/font/google'
+
+// define your variable fonts
+const barlow = Barlow({weight: ['300', '400', '500', '700'], subsets: ['latin']})
+
+export { barlow };
--- Desafio FAI/web/src/styles/theme.ts
+++ Desafio FAI/web/src/styles/theme.ts
@@ -1,21 +1,7 @@
-const theme = {
+export default {
   colors: {
-    blue: '#4684C0',
-    white: '#FFFFFF',
-    black: '#000000'
+    background: "#292929",
+    primary: "#e1e1e6",
+    text: "#e1e1e6",
   },
-  fonts: {
-    primary: {
-      bold: '800 20px Outfit, sans-serif',
-      semi_bold: '600 18px Outfit, sans-serif',
-      regular: '400 16px Sora, sans-serif'
-    },
-    secondary: {
-      bold: '800 20px Open Sans, sans-serif',
-      semi_bold: '600 18px Open Sans, sans-serif',
-      regular: '400 16px Open Sans, sans-serif'
-    }
-  }
 };
-
-export default theme;
