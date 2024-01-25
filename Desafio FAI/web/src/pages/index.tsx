5d5929899d8894cd590295017531cbded2b30649
Ênio Henrique <62400466+ribeirowski@users.noreply.github.com>
2024-01-25 02:56:38 -0300

    desafioFAI: update index.tsx page

--- Desafio FAI/web/src/pages/index.tsx
+++ Desafio FAI/web/src/pages/index.tsx
@@ -1,27 +1,10 @@
-import { Container, ImageContainer } from './style';
-import { Logo } from 'assets';
-import Image from 'next/image';
-import { CustomText } from 'styles/globalComponents';
+import React from "react";
+import { HomeContainer } from "./style";
+import { BlueButton, InputBox, InputBoxCode } from "components";
 
 export default function Home() {
   return (
-    <Container>
-      <ImageContainer>
-        <Image src={Logo} alt="Logo do CITi" />
-      </ImageContainer>
-      <CustomText bold white size="40px" margin="20px 0">
-        NextJS Boilerplate
-      </CustomText>
-      <CustomText regular white secondary>
-        Made with <strong>&lt; &#x0002F; &gt;</strong> and{' '}
-        <strong>&hearts;</strong> by CITi
-      </CustomText>
-    </Container>
+    <HomeContainer>
+    </HomeContainer>
   );
-}
-
-export async function getStaticProps() {
-  return {
-    props: {}
-  };
 }
